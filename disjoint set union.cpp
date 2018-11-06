456b414b8ad176251a124264ae8db89801bfec22
Tanvir Ahmed <34425141+Dragon2050@users.noreply.github.com>
2018-11-06 07:57:29 +0600

    Add files via upload

---
+++ disjoint set union.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int node,edge,par[1000005];
+
+int find(int r)
+{
+    if(par[r] == r)
+        return r;
+    par[r]=find(par[r]);
+}
+
+int main()
+{
+
+    scanf("%d",&node);///number of node.
+    scanf("%d",&edge);///number of edge.
+    for(int i=1;i<=node;i++)
+    {
+        par[i]=i;
+    }
+    for(int i=1;i<=edge;i++)
+    {
+        int u,v;
+        scanf("%d%d",&u,&v);
+        int a=find(u);
+        int b=find(v);
+        if(a==b)
+            ;
+        else
+            par[b]=par[a];
+    }
+    int cnt = 0;
+    for(int i=1;i<=node;i++)
+    {
+        if(par[i]==i)
+            cnt++;
+    }
+    cout<<cnt<<endl;
+    return 0;
+}
